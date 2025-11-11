a08d23c5c8b20e0161c9c7e3e48a8075d2136abd
cookie8monster <161619686+cookie8monster@users.noreply.github.com>
2025-11-11 16:24:55 -0500

    Update fetchStores to use new JSON source

--- Costco.js
+++ Costco.js
@@ -59,7 +59,7 @@ async function initMap(center, zoomLevel) {
 
 async function fetchStores() {
     try {
-        const response = await fetch("https://raw.githubusercontent.com/cookie8monster/GranolaQuest/refs/heads/main/CostcoCombo.json");
+        const response = await fetch("https://raw.githubusercontent.com/cookie8monster/GranolaQuest/refs/heads/main/Nov25UpdatedFullList.json");
         allStores = await response.json();
         renderVisibleStores();
     } catch (error) {
